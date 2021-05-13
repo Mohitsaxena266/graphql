e6b24fbb00e257de8d69260ebc5087aa765c54aa
Mohit Saxena <mohit@hbssweb.com>
2021-05-13 21:13:47 +0530

    GraphQL

---
+++ App.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Home } from './src/Home';
+import { CoffeePage } from './src/CoffeePage';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { View } from 'react-native';
+import { Font } from 'expo';
+
+const MainNavigator = createStackNavigator({
+  Home: { screen: Home },
+  CoffeePage: { screen: CoffeePage },
+}, { headerMode: 'none' });
+
+const AppContainer = createAppContainer(MainNavigator);
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFontsLoaded: false
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'maven-pro-regular': require('./assets/MavenPro-Regular.ttf'),
+      'maven-pro-bold': require('./assets/MavenPro-Bold.ttf')
+    });
+
+    this.setState({ isFontsLoaded: true });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        {this.state.isFontsLoaded ? <AppContainer /> : <View />}
+      </View>
+    );
+  }
+}
